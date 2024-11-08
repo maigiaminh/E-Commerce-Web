bdec9d77f5f0d33195965c62b0a2c3eb1d818e36
maigiaminh <158302350+maigiaminh@users.noreply.github.com>
2024-11-08 17:30:31 +0700

    products details

---
+++ E-Commerce-Web/E-Commerce-Web/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using E_Commerce_Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce_Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly EcommerceContext _context;
+
+        public ProductController()
+        {
+            _context = new EcommerceContext();
+        }
+
+        public ActionResult ProductDetails(int productId)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (product == null)
+            {
+                return HttpNotFound("Product not found.");
+            }
+
+            var relatedProducts = _context.Products
+                .Where(p => p.CategoryID == product.CategoryID && p.ProductID != productId)
+                .OrderByDescending(p => p.CreatedAt)
+                .Take(4)
+                .ToList();
+
+            ViewBag.RelatedProducts = relatedProducts;
+
+            return View(product);
+        }
+    }
+}
