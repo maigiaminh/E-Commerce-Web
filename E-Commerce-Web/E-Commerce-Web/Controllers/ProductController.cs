7e9075f27bb38c1a54dff948c446baffd93302af
maigiaminh <158302350+maigiaminh@users.noreply.github.com>
2024-11-09 13:11:53 +0700

    sort by categories

--- E-Commerce-Web/E-Commerce-Web/Controllers/ProductController.cs
+++ E-Commerce-Web/E-Commerce-Web/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using E_Commerce_Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,22 +17,60 @@ namespace E_Commerce_Web.Controllers
         {
             _context = new EcommerceContext();
         }
-        public ActionResult Shop(int page = 1)
+        public ActionResult Shop(int page = 1, string sortOption = "default", int? categoryId = null)
         {
+            List<Category> categories = _context.Categories.ToList();
+            ViewBag.Categories = categories;
+            ViewBag.SelectedCategoryId = categoryId;
+
+            var products = _context.Products.OrderBy(p => p.ProductID).ToList();
+
+            if (categoryId != null)
+            {
+                products = products.Where(p => p.CategoryID == categoryId).ToList();
+            }
+
+            switch (sortOption)
+            {
+                case "default":
+                    break;
+                case "priceAsc":
+                    products = products.OrderBy(p => p.Price).ToList();
+                    break;
+                case "priceDesc":
+                    products = products.OrderByDescending(p => p.Price).ToList();
+                    break;
+                case "nameAsc":
+                    products = products.OrderBy(p => p.ProductName).ToList();
+                    break;
+                case "nameDesc":
+                    products = products.OrderByDescending(p => p.ProductName).ToList();
+                    break;
+                case "dateAsc":
+                    products = products.OrderBy(p => p.CreatedAt).ToList();
+                    break;
+                case "dateDesc":
+                    products = products.OrderByDescending(p => p.CreatedAt).ToList();
+                    break;
+
+                default:
+                    break;
+            }
+
             int pageSize = 8; 
-            int totalProducts = _context.Products.Count();
+            int totalProducts = products.Count();
             int totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
 
-            var products = _context.Products
-                                    .OrderBy(p => p.ProductID) 
-                                    .Skip((page - 1) * pageSize) 
-                                    .Take(pageSize) 
+            var paginatedProducts = products
+                                    .Skip((page - 1) * pageSize)
+                                    .Take(pageSize)
                                     .ToList();
 
             ViewBag.TotalPages = totalPages;
             ViewBag.CurrentPage = page;
+            ViewBag.SortOption = sortOption;
 
-            return View(products);
+            return View(paginatedProducts);
         }
 
         public ActionResult ProductDetails(int productId)
