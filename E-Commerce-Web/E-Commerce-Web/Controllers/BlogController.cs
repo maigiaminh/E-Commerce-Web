80dd32c1b702340592eaf9b6db4c27091669d838
maigiaminh <158302350+maigiaminh@users.noreply.github.com>
2024-11-17 03:25:56 +0700

    blog controller

--- E-Commerce-Web/E-Commerce-Web/Controllers/BlogController.cs
+++ E-Commerce-Web/E-Commerce-Web/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
 ﻿using E_Commerce_Web.Models;
+using iText.StyledXmlParser.Jsoup.Nodes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,21 +16,66 @@ namespace E_Commerce_Web.Controllers
             _context = new EcommerceContext();
         }
 
-        public ActionResult Index()
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var blogs = _context.Blogs.OrderBy(p => p.Id).ToList();
+            int pageSize = 4;
+            int totalBlogs = blogs.Count();
+            int totalPages = (int)Math.Ceiling((double)totalBlogs / pageSize);
+
+            var paginatedBlogs = blogs
+                                    .Skip((page - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToList();
+
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = page;
+
+            return View(paginatedBlogs);
         }
 
-        public ActionResult BlogDetail(int id = 0)
+        public ActionResult BlogDetail(int id)
         {
-            //var blog = _context.Include("Comments").FirstOrDefault(b => b.Id == id);
+            var blog = _context.Blogs.FirstOrDefault(b => b.Id == id);
 
-           // if (blog == null)
-            //{
-                //return HttpNotFound();
-           // }
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
 
-            return View();
+            var recentBlogs = _context.Blogs
+                .Where(b => b.Id != id)
+                .OrderByDescending(b => b.CreatedAt)
+                .Take(5)
+                .ToList();
+
+            ViewBag.RecentBlogs = recentBlogs;
+            return View(blog);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(int BlogId, string Content)
+        {
+            if (string.IsNullOrWhiteSpace(Content))
+            {
+                return RedirectToAction("Detail", new { id = BlogId });
+            }
+
+            if (Session["UserID"] != null)
+            {
+                int userID = Convert.ToInt32(Session["UserID"]);
+                var comment = new E_Commerce_Web.Models.Comment
+                {
+                    BlogId = BlogId,
+                    UserId = userID,
+                    Content = Content,
+                    CreatedAt = DateTime.Now
+                };
+                _context.Comments.Add(comment);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Detail", new { id = BlogId });
         }
     }
 }
--- E-Commerce-Web/E-Commerce-Web/Models/Blog/Comment.cs
+++ E-Commerce-Web/E-Commerce-Web/Models/Blog/Comment.cs
@@ -11,7 +11,7 @@ namespace E_Commerce_Web.Models
         [Key]
         public int Id { get; set; }
         public int BlogId { get; set; }
-        public string UserId { get; set; }
+        public int UserId { get; set; }
         public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
 
